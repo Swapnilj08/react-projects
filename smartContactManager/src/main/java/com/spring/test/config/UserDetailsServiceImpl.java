c0f2c6176813e19d5502a49d5d52d805fbd27edc
Swapnilj08 <50283080+Swapnilj08@users.noreply.github.com>
2021-10-07 23:03:29 +0530

    initial commit

---
+++ smartContactManager/src/main/java/com/spring/test/config/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.spring.test.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.spring.test.DAO.UserRepository;
+import com.spring.test.entities.User;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		//fetching user from database
+		User user = userRepository.getUserByName(username);
+		if (user==null) {
+			throw new UsernameNotFoundException("Could not Found User");
+		}
+		 CostumUserDetails userDetails = new CostumUserDetails(user);
+		return userDetails;
+	}
+
+}

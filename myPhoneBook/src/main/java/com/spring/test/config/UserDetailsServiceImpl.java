e579a68cbf2e1e5aa8d0f45b94a0e643ea4c7494
swapnil <50283080+Swapnilj08@users.noreply.github.com>
2021-10-23 19:47:48 +0530

    myPhoneBook

---
+++ myPhoneBook/src/main/java/com/spring/test/config/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.spring.test.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.spring.test.DAO.UserRepository;
+import com.spring.test.entities.User;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		//fetching user from database
+		User user = userRepository.getUserByName(username);
+		
+		System.out.println("UserDetailsServiceImpl class"+user);
+		if (user==null) {
+			throw new UsernameNotFoundException("Could not Found User");
+		}
+		 CostumUserDetails userDetails = new CostumUserDetails(user);
+		return userDetails;
+	}
+
+}
